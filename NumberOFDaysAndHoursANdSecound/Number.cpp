aa45be8d372e9bb568a97e2ebf3878520897fee1
hassanelsherif90 <hassanelsherif90.com>
2023-07-08 11:46:23 +0300

    Update

--- NumberOFDaysAndHoursANdSecound/Number.cpp
+++ NumberOFDaysAndHoursANdSecound/Number.cpp
@@ -8,14 +8,14 @@ bool IsYearLeap( short Year )
     return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
 }
 
-int NumberDays(short Year)
+int NumberOfDays(short Year)
 {
     return ( IsYearLeap( Year ) ? 366 : 365);
 }
 
 int NumberOfHours(short Year)
 {
-    return (NumberDays(Year) * 24 );
+    return (NumberOfDays(Year) * 24 );
 }
 
 int NumberOfMinutes(short Year)
@@ -38,10 +38,10 @@ short ReadYears()
 
 void PrintAll(short Year)
 {
-    cout << "\nNumber Of Days in Year [ " <<  Year << "] : " << NumberDays(Year) << endl;
-    cout << "\nNumber Of Hours in Year [ " <<  Year << "] : " << NumberOfHours(Year) << endl;
-    cout << "\nNumber Of Minutes in Year [ " <<  Year << "] : " << NumberOfMinutes(Year)<< endl;
-    cout << "\nNumber Of Seconds in Year [ " <<  Year << "] : " <<NumberOfSeconds(Year) << endl;
+        cout << "Number Of Days in Year     [ " << Year << "] : " << NumberOfDays(Year) << endl;
+        cout << "Number Of Hours in Year    [ " << Year << "] : " << NumberOfHours(Year) << endl;
+        cout << "Number Of Minutes in Year  [ " << Year << "] : " << NumberOfMinutes(Year)<< endl;
+        cout << "Number Of Seconds in Year  [ " << Year << "] : " <<NumberOfSeconds(Year) << endl << endl;
 }
 
 int main()
@@ -49,5 +49,5 @@ int main()
     short Year = ReadYears();
     PrintAll(Year);
 
-    system("pause>0");
+    // system("pause>0");
 }
